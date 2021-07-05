6e9e722761b60d4ee3dcd2d5177310c5e1328553
y0913 <yohei19910825@gmail.com>
2021-07-06 02:07:06 +0900

    update: コントローラ 作成

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,10 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 // 登録されている商品を全て取得
-router.get('/', asyncHandler(async (req,res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
-
+router.route('/').get(getProducts)
 // 個別の商品データを取得
-router.get('/:id', asyncHandler( async (req,res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-    
-}))
+router.route('/:id').get(getProductById)
 
 export default router
