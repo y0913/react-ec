6b7757295e82b97625a5c6d4bfcb3f290a4e86ad
y0913 <yohei19910825@gmail.com>
2021-07-06 20:10:19 +0900

    update: tokenを使用した認証を実装

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc  ユーザ認証とトークン取得
 // @route POST /api/users/login
@@ -14,7 +15,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -22,4 +23,44 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser }
+// @desc  新規ユーザ作成
+// @route POST /api/users
+const registerUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    // res.send({email, password})
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('無効なパスワードです。')
+    }
+})
+
+// @desc  ユーザ情報を取得
+// @route GET /api/users/profile
+const getUserProfile = asyncHandler(async (req, res) => {
+    
+    const user = await User.findById(req.user._id)
+    
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+export { authUser, getUserProfile }
