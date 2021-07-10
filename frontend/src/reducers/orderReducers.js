ddd6cf61e0652943b2bc0f4e440b2c425e400392
y0913 <yohei19910825@gmail.com>
2021-07-11 03:32:14 +0900

    update: ログアウト時にユーザの情報をリセットする処理を追加

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -12,6 +12,7 @@ import {
     ORDER_MYLIST_REQUEST,
     ORDER_MYLIST_SUCCESS,
     ORDER_MYLIST_FAIL,
+    ORDER_MYLIST_RESET,
 } from "../constants/orderConstants"
 
 export const orderCreateReducer = (state={}, action) => {
@@ -63,6 +64,8 @@ export const orderMyListReducer = (state = { orders: [] }, action) => {
             return { loading: false, orders: action.payload }
         case ORDER_MYLIST_FAIL:
             return { loading: false, error: action.payload }
+        case ORDER_MYLIST_RESET:
+            return { orders: [] }
         default:
             return state
     }
