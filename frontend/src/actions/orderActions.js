ee6b71ca9faff243c99fc229eb0853680ccd4d26
y0913 <yohei19910825@gmail.com>
2021-07-11 01:59:55 +0900

    update: 支払い状況更新のAPIを作成

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from "../constants/orderConstants"
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS } from "../constants/orderConstants"
 
 export const createOrder = (order) => async (dispatch, getState) => {
     try {
@@ -56,6 +56,37 @@ export const getOrderDetails = (orderId) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: ORDER_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_PAY_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+ 
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`,
+            }
+        }
+
+        const { data } = await axios.put(`/api/orders/${orderId}/pay`, paymentResult,config)
+
+        // ユーザ詳細取得成功
+        dispatch({
+            type: ORDER_PAY_SUCCESS,
+            payload: data 
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_PAY_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
