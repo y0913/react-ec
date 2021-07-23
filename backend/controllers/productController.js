ca47f37665a2f2d4d31ee545adeb72d91ea5afb6
y0913 <yohei19910825@gmail.com>
2021-07-24 08:21:44 +0900

    update: paginationを作成

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,15 +2,23 @@ import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler(async (req, res) => {
-    const keyword = req.query.keyword ? {
+    const pageSize = 10
+    const page = Number(req.query.pageNumber) || 1
+    const keyword = req.query.keyword
+    ? {
         name: {
-            $regex: req.query.keyword,
-            $options: 'i'
-        }
-    } : {}
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {}
 
-    const products = await Product.find({ ...keyword })
-    res.json(products)
+  const count = await Product.countDocuments({ ...keyword })
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 const getProductById = asyncHandler(async (req, res) => {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,31 +4,40 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import Paginate from '../components/Paginate'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = ({ match }) => {
     const keyword = match.params.keyword
+    const pageNumber = match.params.pageNumber || 1
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     useEffect(() => {
-        dispatch(listProducts(keyword))
-    }, [dispatch, keyword])
+        dispatch(listProducts(keyword, pageNumber))
+    }, [dispatch, keyword, pageNumber])
 
     return (
         <>
-            <h1>latest products</h1>
-            {loading ? <Loader /> : error ? <Message variant='danger' /> : 
-                <Row>
-                    {products.map(product => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))}
-                </Row>
-            }
+            <h1>商品一覧</h1>
+            {loading ? <Loader /> : error ? <Message variant='danger' /> : (
+                <>
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                    <Paginate
+                        pages={pages}
+                        page={page}
+                        keyword={keyword ? keyword : ''}
+                    />
+                </>
+            )}
         </>
     )
 }
