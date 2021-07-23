416c004525ce7d75b0bcf35ce1d4cb41de4f686e
y0913 <yohei19910825@gmail.com>
2021-07-23 14:20:53 +0900

    update: 配達済みに設定する画面を作成

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,25 @@
 import axios from "axios"
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_MYLIST_FAIL, ORDER_MYLIST_REQUEST, ORDER_MYLIST_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS } from "../constants/orderConstants"
+import {
+    ORDER_CREATE_FAIL,
+    ORDER_CREATE_REQUEST,
+    ORDER_CREATE_SUCCESS,
+    ORDER_DETAILS_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_LIST_FAIL,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
+    ORDER_MYLIST_FAIL,
+    ORDER_MYLIST_REQUEST,
+    ORDER_MYLIST_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS,
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_RESET,
+} from "../constants/orderConstants"
 
 export const createOrder = (order) => async (dispatch, getState) => {
     try {
@@ -147,6 +167,36 @@ export const listOrders = () => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: ORDER_LIST_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_DELIVER_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+ 
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            }
+        }
+
+        const { data } = await axios.put(`/api/orders/${order._id}/deliver`, {}, config)
+
+        // ユーザ詳細取得成功
+        dispatch({
+            type: ORDER_DELIVER_SUCCESS,
+            payload: data 
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_DELIVER_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -46,7 +46,7 @@ const OrderListScreen = ({ history }) => {
                              <td>{order.createdAt.substring(0, 10)}</td>
                              <td>{order.totalPrice}</td>
                              <td>{order.isPaid ? (order.paidAt.substring(0, 10)) : (<i className='fas fa-times' style={{color: 'red'}}></i>)}</td>
-                             <td>{order.isDeliverd ? (order.isDeliverd.substring(0, 10)) : (<i className='fas fa-times' style={{color: 'red'}}></i>)}</td>
+                             <td>{order.isDeliverd ? (order.deliverAt.substring(0, 10)) : (<i className='fas fa-times' style={{color: 'red'}}></i>)}</td>
                              <td>
                                  <LinkContainer to={`/order/${order._id}/edit`}>
                                      <Button variant='light' className='btn-sm'>
